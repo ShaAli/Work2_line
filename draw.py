87743ad10021732a439ebf3570c7cb071708bf74
ShaAli <sali1@stuy.edu>
2017-02-16 00:51:51 -0500

    other octants added

--- draw.py
+++ draw.py
@@ -1,7 +1,7 @@
 from display import *
 
 #octant 1
-def draw_line( x0, y0, x1, y1, screen, color ):
+def draw_line1( x0, y0, x1, y1, screen, color ):
     x = x0
     y = y0
     A = y1-y
@@ -14,3 +14,110 @@ def draw_line( x0, y0, x1, y1, screen, color ):
             d = d + 2*B
         x = x + 1
         d = d + 2*A
+
+#octant 2
+def draw_line2( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1-y
+    B = x-x1
+    d = A + 2*B
+    while (y < y1):
+        plot(screen, color, x, y)
+        if d < 0:
+            x = x+1
+            d = d + 2*A
+        y = y + 1
+        d = d + 2*B
+
+#octant 3
+def draw_line3( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1-y
+    B = x-x1
+    d = A + 2*B
+    while (y < y1):
+        plot(screen, color, x, y)
+        if d < 0:
+            x = x-1
+            d = d + 2*A
+        y = y + 1
+        d = d - 2*B
+
+#octant 4
+def draw_line4( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1-y
+    B = x-x1
+    d = 2*A + B
+    while (x > x1):
+        plot(screen, color, x, y)
+        if d > 0:
+            y = y+1
+            d = d - 2*B
+        x = x - 1
+        d = d + 2*A
+
+#octant 5
+def draw_line5( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1-y
+    B = x-x1
+    d = 2*A - B
+    while (x > x1):
+        plot(screen, color, x, y)
+        if d < 0:
+            y = y-1
+            d = d + 2*B
+        x = x - 1
+        d = d + 2*A
+
+#octant 6
+def draw_line6( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1-y
+    B = x-x1
+    d = A - 2*B
+    while (y > y1):
+        plot(screen, color, x, y)
+        if d > 0:
+            x = x-1
+            d = d + 2*A
+        y = y - 1
+        d = d + 2*B
+
+#octant 7
+def draw_line7( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1-y
+    B = x-x1
+    d = A - 2*B
+    while (y > y1):
+        plot(screen, color, x, y)
+        if d > 0:
+            x = x+1
+            d = d + 2*A
+        y = y - 1
+        d = d - 2*B
+
+#octant 8
+def draw_line8( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1-y
+    B = x-x1
+    d = 2*A - B
+    while (x < x1):
+        plot(screen, color, x, y)
+        if d < 0:
+            y = y-1
+            d = d - 2*B
+        x = x + 1
+        d = d + 2*A
+
+
